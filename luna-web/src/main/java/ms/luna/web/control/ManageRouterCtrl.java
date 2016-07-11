b3ea0f1eb8247781b7b614a780d4063315f08b09
chenshangan <shawn@visualbusiness.com>
2016-07-11 17:09:27 +0800

    New feature. Add router manageCtrl

---
+++ luna-web/src/main/java/ms/luna/web/control/ManageRouterCtrl.java
@@ -0,0 +1,36 @@
+package ms.luna.web.control;
+
+import ms.luna.web.common.BasicCtrl;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * Copyright (C) 2015 - 2016 MICROSCENE Inc., All Rights Reserved.
+ *
+ * @Author: shawn@visualbusiness.com
+ * @Date: 2016-07-11
+ */
+
+@Controller("manageRouterCtrl")
+@RequestMapping("/manage_router.do")
+public class ManageRouterCtrl extends BasicCtrl {
+
+    public static final String INIT = "method=init";
+
+    @RequestMapping(params = INIT)
+    public ModelAndView init(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        HttpSession session = request.getSession(false);
+        if (session != null) {
+            session.setAttribute("menu_selected", "manage_router");
+        }
+        ModelAndView modelAndView = buildModelAndView("/manage_router");
+
+        return modelAndView;
+    }
+}
