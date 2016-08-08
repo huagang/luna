f7e573c9a8d2282aff31d292e51db59f1d67680b
chenshangan <shawn@visualbusiness.com>
2016-08-08 15:04:49 +0800

    New process. finish updateUserRole

---
+++ luna-web/src/main/java/ms/luna/web/common/CommonURI.java
@@ -0,0 +1,13 @@
+package ms.luna.web.common;
+
+/**
+ * Copyright (C) 2015 - 2016 MICROSCENE Inc., All Rights Reserved.
+ *
+ * @Author: shawn@visualbusiness.com
+ * @Date: 2016-08-08
+ */
+public class CommonURI {
+
+    public static final String LOGIN_SERVLET_PATH = "/common/login";
+    public static final String AUTH_FAIL_SERVLET_PATH = "/common/authFail";
+}
