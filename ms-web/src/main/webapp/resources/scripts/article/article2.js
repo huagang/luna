b73a9b4a0490c056e7bd3fe8b83336f99055d675
Victor <victor@visualbusiness.com>
2016-08-25 18:03:44 +0800

    修改返回顶部时间

--- ms-web/src/main/webapp/resources/scripts/article/article2.js
+++ ms-web/src/main/webapp/resources/scripts/article/article2.js
@@ -240,7 +240,7 @@ function pageScroll(e) {
     var height = 0 - document.body.clientHeight / 10;
     window.scrollBy(0, height);
     //延时递归调用，模拟滚动向上效果
-    scrolldelay = setTimeout('pageScroll()', 50);
+    scrolldelay = setTimeout('pageScroll()', 10);
     //获取scrollTop值，声明了DTD的标准网页取document.documentElement.scrollTop，否则取document.body.scrollTop；因为二者只有一个会生效，另一个就恒为0，所以取和值可以得到网页的真正的scrollTop值
     var sTop = document.documentElement.scrollTop + document.body.scrollTop;
     //判断当页面到达顶部，取消延时代码（否则页面滚动到顶部会无法再向下正常浏览页面）
