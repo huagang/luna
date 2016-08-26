997d823063873b3db6d434f802520ee16aadd1d9
chenshangan <shawn@visualbusiness.com>
2016-08-26 14:32:12 +0800

    Improvement. finish login api

--- luna-web/src/main/java/ms/luna/web/control/api/AuthController.java
+++ luna-web/src/main/java/ms/luna/web/control/api/AuthController.java
@@ -3,6 +3,7 @@ package ms.luna.web.control.api;
 import com.alibaba.fastjson.JSONObject;
 import ms.luna.biz.cons.ErrorCode;
 import ms.luna.biz.sc.LunaUserService;
+import ms.luna.biz.table.LunaUserTable;
 import ms.luna.biz.util.FastJsonUtil;
 import ms.luna.web.util.TokenUtil;
 import org.apache.log4j.Logger;
@@ -46,6 +47,29 @@ public class AuthController {
             }
         } catch (Exception ex) {
             logger.error("Failed to verify token", ex);
+            return FastJsonUtil.error(ErrorCode.INTERNAL_ERROR, "内部错误");
+        }
+
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/login")
+    @ResponseBody
+    public JSONObject login(@RequestParam(value="luna_name", required=true) String lunaName,
+                                  @RequestParam(value="password", required=true) String password) {
+        try {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put(LunaUserTable.FIELD_LUNA_NAME, lunaName);
+            jsonObject.put(LunaUserTable.FIELD_PASSWORD, password);
+            JSONObject result = lunaUserService.loginUser(jsonObject);
+
+            if(! result.getString("code").equals("0")) {
+                return FastJsonUtil.error(ErrorCode.INVALID_PARAM, "用户名或密码错误");
+            }
+            JSONObject data = result.getJSONObject("data");
+            return FastJsonUtil.sucess("success", data);
+
+        } catch (Exception ex) {
+            logger.error("Failed to login", ex);
             return FastJsonUtil.error(ErrorCode.INTERNAL_ERROR, "内部错误");
         }
 
