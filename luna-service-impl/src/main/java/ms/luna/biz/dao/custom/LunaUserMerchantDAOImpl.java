070eb5555e0c582bf77eaffb7207500051993857
greek <greek@visualbusiness.com>
2016-09-02 21:40:24 +0800

    New feature. add new table: luna_user_merchant

---
+++ luna-service-impl/src/main/java/ms/luna/biz/dao/custom/LunaUserMerchantDAOImpl.java
@@ -0,0 +1,8 @@
+package ms.luna.biz.dao.custom;
+
+import ms.luna.biz.dao.LunaUserMerchantDAOBaseImpl;
+import org.springframework.stereotype.Repository;
+
+@Repository("lunaUserMerchantDAO")
+public class LunaUserMerchantDAOImpl extends LunaUserMerchantDAOBaseImpl implements LunaUserMerchantDAO {
+}
