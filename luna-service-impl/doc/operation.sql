890b52b8e302be0bb8f56a2f71196ea8452b5092
greek <greek@visualbusiness.com>
2016-09-06 20:33:39 +0800

    operation.sql 整理

--- luna-service-impl/doc/operation.sql
+++ luna-service-impl/doc/operation.sql
@@ -1,39 +1,4 @@
 
-CREATE TABLE `ms_operation_log` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `resource_id` varchar(32) NOT NULL COMMENT '资源id',
-  `type` varchar(8) NOT NULL COMMENT '资源类型',
-  `note` varchar(255) DEFAULT NULL COMMENT '备注',
-  `unique_id` varchar(32) NOT NULL COMMENT '用户id',
-  `regist_hhmmss` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-create table `ms_route` (
-  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '线路id',
-  `name` varchar(64) NOT NULL COMMENT '线路名称',
-  `business_id` int(11) NOT NULL COMMENT '所属业务id',
-  `description` varchar(1024) DEFAULT '' COMMENT '线路介绍',
-  `cost_id` int(1) NOT NULL COMMENT '体力消耗值',
-  `cover` varchar(255) DEFAULT '' COMMENT '封面图',
-  `unique_id` char(32) NOT NULL COMMENT '创建人id',
-  `regist_hhmmss` timestamp NULL DEFAULT NULL,
-  `up_hhmmss` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `name` (`name`),
-  KEY `business_id` (`business_id`)
-)ENGINE=InnoDB default charset=utf8 comment='线路表';
-
-# 增加 线路管理 菜单
-insert into luna_menu (`id`,`name`,`code`,`module_id`,`display_order`) values(20,"线路管理","route",4,7);
-insert into luna_role_menu (`role_id`,`menu_id`) values(1,20);
-insert into luna_role_menu (`role_id`,`menu_id`) values(2,20);
-insert into luna_role_menu (`role_id`,`menu_id`) values(3,20);
-insert into luna_role_menu (`role_id`,`menu_id`) values(8,20);
-insert into luna_role_menu (`role_id`,`menu_id`) values(9,20);
-
-
-
 CREATE TABLE `luna_goods_category` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '类别id',
   `name` varchar(32) NOT NULL COMMENT '类别名称',
@@ -47,14 +12,6 @@ CREATE TABLE `luna_goods_category` (
   KEY `root_idx` (`root`),
   CONSTRAINT `root` FOREIGN KEY (`root`) REFERENCES `luna_goods_category` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='商品类别表';
-
-
-INSERT INTO luna_menu` (`name`, `code`, `module_id`, `display_order`, `status`) VALUES ('商品类别管理', 'goodsCategory', '3', '2', '1');
-UPDATE luna_menu` SET `display_order`='3' WHERE `id`='10';
-UPDATE `luna_menu` SET `display_order`='4' WHERE `id`='11';
-UPDATE `luna_menu` SET `display_order`='5' WHERE `id`='12';
-
-INSERT INTO luna_role_menu` (`role_id`, `menu_id`) VALUES ('1', (SELECT `luna_menu`.id FROM `luna_menu` WHERE `luna_menu`.name = "商品类别管理"));
 
 
 CREATE TABLE `luna_trade_application` (
@@ -86,8 +43,12 @@ CREATE TABLE `luna_trade_application` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户交易申请表';
 
 
-ALTER TABLE `ms_merchant_manage`
-ADD COLUMN `trade_status` INT NOT NULL DEFAULT 0 COMMENT '商户交易直通车开通状态' AFTER `updated_by_unique_id`;
+INSERT INTO luna_menu` (`name`, `code`, `module_id`, `display_order`, `status`) VALUES ('商品类别管理', 'goodsCategory', '3', '2', '1');
+UPDATE luna_menu` SET `display_order`='3' WHERE `id`='10';
+UPDATE `luna_menu` SET `display_order`='4' WHERE `id`='11';
+UPDATE `luna_menu` SET `display_order`='5' WHERE `id`='12';
+INSERT INTO luna_role_menu` (`role_id`, `menu_id`) VALUES ('1', (SELECT `luna_menu`.id FROM `luna_menu` WHERE `luna_menu`.name = "商品类别管理"));
+
 
 INSERT INTO `luna_menu` (`name`, `code`, `module_id`, `status`) VALUES ('交易直通车', 'tradeApplication', '3', '1');
 UPDATE `luna_menu` SET `display_order`='2' WHERE `id`='9';
@@ -107,30 +68,12 @@ alter table ms_column drop index code;
 alter table ms_column add index(name);
 alter table ms_column change category_id category_id varchar(32) default null;
 
-
 update luna_menu set auth='pano-viewer:login:*,pano-viewer:album:*,pano-viewer:pano:*', url='http://pano.visualbusiness.cn/backstage/htmls/albumEdit.html' where id=7;
 update ms_column, ms_article set ms_column.business_id = ms_article.business_id where ms_column.id=ms_article.column_id;
-
-ALTER TABLE ms_merchant_manage ADD trade_status int(11) NOT NULL DEFAULT '0' COMMENT '商户交易直通车开通状态';
-ALTER TABLE ms_merchant_manage ADD unique_id char(32) default NULL comment "关联用户的id";
-ALTER TABLE ms_merchant_manage ADD unique (`unique_id`);
 
 ALTER TABLE ms_show_app drop index `app_name`;
 ALTER TABLE ms_show_app drop INDEX `business_id`;
 ALTER TABLE ms_show_app add UNIQUE(business_id, app_name);
-
-update ms_farm_field set limits ="{\"PIC\":[{\"empty\":true,\"max\":20,\"ext\":[\"png\",\"jpg\"]}]}" where name = "start_page_foreground_pic";
-update ms_farm_field set limits ="{\"PIC\":[{\"empty\":true,\"max\":20,\"ext\":[\"png\",\"jpg\"]}]}" where name = "start_page_background_pic";
-update ms_farm_field set limits ="{\"PIC\":[{\"empty\":true,\"max\":20,\"ext\":[\"png\",\"jpg\"]}]}" where name = "manager_pic";
-update ms_farm_field set limits ="{\"TEXT\":[{\"empty\":true,\"max\":255}]}" where name = "all_chosen_room_panorama_type";
-update ms_farm_field set limits ="{\"PIC\":[{\"empty\":true,\"max\":20,\"ext\":[\"png\",\"jpg\"]}],\"TEXT\":[{\"empty\":true,\"max\":255}],\"num\":{\"min\":3}}" where name = "delicacy";
-update ms_farm_field set limits ="{\"TEXT\":[{\"empty\":true,\"max\":255}]}" where name = "country_enjoyment";
-update ms_farm_field set limits="{\"CHECKBOX\":[{\"empty\":true}]}" where name = "facility";
-
-alter table ms_route drop index `name`;
-alter table ms_route add unique key `name` (`business_id`, `name`);
-UPDATE luna_menu SET status=0 WHERE code='business';
-
 
 update luna_menu set auth='pano-viewer:login:*,pano-viewer:album:*,pano-viewer:pano:*', url='http://pano.visualbusiness.cn/backstage/htmls/albumEdit.html' where id=7;
 
@@ -150,6 +93,13 @@ UPDATE `luna_menu` SET `code`='goodsCategory' WHERE `id`='5';
 
 DELETE FROM `luna_menu` WHERE `name`='商品类别管理';
 
+-- 商户表
+ALTER TABLE `ms_merchant_manage`
+ADD COLUMN `trade_status` INT NOT NULL DEFAULT 0 COMMENT '商户交易直通车开通状态' AFTER `updated_by_unique_id`;
+
+ALTER TABLE ms_merchant_manage ADD unique_id char(32) default NULL comment "关联用户的id";
+ALTER TABLE ms_merchant_manage ADD unique (`unique_id`);
+
 alter table ms_merchant_manage add  `application_id` int(11) default NULL COMMENT '申请id';
 alter table ms_merchant_manage add  `idcard_pic_url` varchar(200) default NULL COMMENT '申请人身份证正反面照片地址';
 alter table ms_merchant_manage add  `idcard_period` varchar(26) default NULL COMMENT '申请人身份证有效期';
@@ -164,3 +114,16 @@ alter table ms_merchant_manage add  `account_no` varchar(20) default NULL COMMEN
 alter table ms_merchant_manage add  `app_status` int(11) default NULL COMMENT '申请状态';
 alter table ms_merchant_manage add  `account_province` varchar(36) default NULL;
 
+-- 农家表
+update ms_farm_field set limits ="{\"PIC\":[{\"empty\":true,\"max\":20,\"ext\":[\"png\",\"jpg\"]}]}" where name = "start_page_foreground_pic";
+update ms_farm_field set limits ="{\"PIC\":[{\"empty\":true,\"max\":20,\"ext\":[\"png\",\"jpg\"]}]}" where name = "start_page_background_pic";
+update ms_farm_field set limits ="{\"PIC\":[{\"empty\":true,\"max\":20,\"ext\":[\"png\",\"jpg\"]}]}" where name = "manager_pic";
+update ms_farm_field set limits ="{\"TEXT\":[{\"empty\":true,\"max\":255}]}" where name = "all_chosen_room_panorama_type";
+update ms_farm_field set limits ="{\"PIC\":[{\"empty\":true,\"max\":20,\"ext\":[\"png\",\"jpg\"]}],\"TEXT\":[{\"empty\":true,\"max\":255}],\"num\":{\"min\":3}}" where name = "delicacy";
+update ms_farm_field set limits ="{\"TEXT\":[{\"empty\":true,\"max\":255}]}" where name = "country_enjoyment";
+update ms_farm_field set limits="{\"CHECKBOX\":[{\"empty\":true}]}" where name = "facility";
+
+-- 线路管理
+alter table ms_route drop index `name`;
+alter table ms_route add unique key `name` (`business_id`, `name`);
+UPDATE luna_menu SET status=0 WHERE code='business';
